e6afb48681129a62e7b4e3619e3c686fb0e9c97b
chijitui <johnxiao675@gmail.com>
2019-04-05 20:42:01 +0800

    Init：项目初始化

---
+++ src/binary-search/simple.ts
@@ -0,0 +1,31 @@
+const binarySearch = (arr: number[], target: number) => {
+  let low = 0;
+  let high = arr.length - 1;
+  let current = averageFloor(low, high);
+
+  while (target !== arr[current]) {
+
+    if (current <= 0 || current >= high) {
+      return -1;
+    }
+
+    if (target > arr[current]) {
+      low = current;
+      current = averageFloor(current, high);
+    } else {
+      high = current;
+      current = averageFloor(low, current);
+    }
+  }
+
+  return current;
+}
+
+const averageFloor = (num1: number, num2: number) => {
+  return Math.floor((num1 + num2) / 2);
+}
+
+const testArr = [1, 5, 6, 8, 9, 11];
+
+console.log(binarySearch(testArr, 9)); // 4
+console.log(binarySearch(testArr, 2)); // -1
